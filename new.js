00315f3a015337b622a615ca47ace803c4a76a1b
zohaib-knysys <101108646+zohaib-knysys@users.noreply.github.com>
2022-03-17 16:43:12 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,303 @@
+import gsap from "https://cdn.skypack.dev/gsap";
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js";
+
+// import * as dat from "https://cdn.skypack.dev/dat.gui"
+
+// const gui = new dat.GUI()
+
+const world = {
+  plane: {
+    width: 400,
+    height: 400,
+    widthSegments: 80,
+    heightSegments: 80,
+  },
+};
+
+// gui.add(world.plane, 'width', 1, 500).onChange(generatePlane)
+// gui.add(world.plane, 'height', 1, 500).onChange(generatePlane)
+// gui.add(world.plane, 'widthSegments', 1, 100).onChange(generatePlane)
+// gui.add(world.plane, 'heightSegments', 1, 100).onChange(generatePlane)
+
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  );
+
+  // vertice position randomization
+  const { array } = planeMesh.geometry.attributes.position;
+  const randomValues = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i % 3 === 0) {
+      const x = array[i];
+      const y = array[i + 1];
+      const z = array[i + 2];
+
+      array[i] = x + (Math.random() - 0.5) * 3;
+      array[i + 1] = y + (Math.random() - 0.5) * 3;
+      array[i + 2] = z + (Math.random() - 0.5) * 3;
+    }
+
+    randomValues.push(Math.random() * Math.PI * 2);
+  }
+
+  planeMesh.geometry.attributes.position.randomValues = randomValues;
+  planeMesh.geometry.attributes.position.originalPosition =
+    planeMesh.geometry.attributes.position.array;
+
+  const colors = [];
+  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(0.6, 0, 0);
+  }
+
+  planeMesh.geometry.setAttribute(
+    "color",
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  );
+}
+
+const raycaster = new THREE.Raycaster();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
+camera.position.z = 50;
+
+const planeGeometry = new THREE.PlaneGeometry(
+  world.plane.width,
+  world.plane.height,
+  world.plane.widthSegments,
+  world.plane.heightSegments
+);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
+  vertexColors: true,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+generatePlane();
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, -1, 1);
+scene.add(light);
+
+const backLight = new THREE.DirectionalLight(0xffffff, 1);
+backLight.position.set(0, 0, -1);
+scene.add(backLight);
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+let frame = 0;
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  raycaster.setFromCamera(mouse, camera);
+  frame += 0.02;
+
+  const { array, originalPosition, randomValues } =
+    planeMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    // x
+    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.04;
+
+    // y
+    array[i + 1] =
+      originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * 0.001;
+  }
+
+  planeMesh.geometry.attributes.position.needsUpdate = true;
+
+  const intersects = raycaster.intersectObject(planeMesh);
+  if (intersects.length > 0) {
+    const { color } = intersects[0].object.geometry.attributes;
+
+    // vertice 1
+    color.setX(intersects[0].face.a, 0.1);
+    color.setY(intersects[0].face.a, 0.5);
+    color.setZ(intersects[0].face.a, 1);
+
+    // vertice 2
+    color.setX(intersects[0].face.b, 0.1);
+    color.setY(intersects[0].face.b, 0.5);
+    color.setZ(intersects[0].face.b, 1);
+
+    // vertice 3
+    color.setX(intersects[0].face.c, 0.1);
+    color.setY(intersects[0].face.c, 0.5);
+    color.setZ(intersects[0].face.c, 1);
+
+    intersects[0].object.geometry.attributes.color.needsUpdate = true;
+
+    const initialColor = {
+      r: 0.6,
+      g: 0,
+      b: 0,
+    };
+
+    const hoverColor = {
+      r: 1,
+      g: 0,
+      b: 0.1,
+    };
+
+    gsap.to(hoverColor, {
+      r: initialColor.r,
+      g: initialColor.g,
+      b: initialColor.b,
+      duration: 2,
+      onUpdate: () => {
+        // vertice 1
+        color.setX(intersects[0].face.a, hoverColor.r);
+        color.setY(intersects[0].face.a, hoverColor.g);
+        color.setZ(intersects[0].face.a, hoverColor.b);
+
+        // vertice 2
+        color.setX(intersects[0].face.b, hoverColor.r);
+        color.setY(intersects[0].face.b, hoverColor.g);
+        color.setZ(intersects[0].face.b, hoverColor.b);
+
+        // vertice 3
+        color.setX(intersects[0].face.c, hoverColor.r);
+        color.setY(intersects[0].face.c, hoverColor.g);
+        color.setZ(intersects[0].face.c, hoverColor.b);
+        color.needsUpdate = true;
+      },
+    });
+  }
+}
+
+animate();
+
+addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+});
+
+function init() {
+  gsap.to(camera.position, {
+    duration: 1,
+    x: 0,
+    y: 0,
+    z: 4,
+  });
+
+  gsap.to(camera.rotation, {
+    duration: 2,
+    x: Math.PI / 2,
+    y: 0,
+    z: 0,
+  });
+
+  gsap.to(camera.position, {
+    delay: 2,
+    duration: 0.7,
+    x: 0,
+    y: 200,
+    z: 30,
+  });
+  inits();
+	addSphere();
+	render();
+}
+
+
+
+var  stars=[];
+	 
+    //assign three.js objects to each variable
+    function inits(){
+      var camera, scene, renderer;
+      //camera
+      camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+      camera.position.z = 5;	 
+  
+      //scene
+      scene = new THREE.Scene();
+       
+      //renderer
+      renderer = new THREE.WebGLRenderer();
+      //set the size of the renderer
+      renderer.setSize( window.innerWidth, window.innerHeight );
+       
+      //add the renderer to the html document body
+      document.body.appendChild( renderer.domElement );
+    }
+  
+  
+    function addSphere(){
+  
+          for ( var z= -1000; z < 1000; z+=15 ) {
+      
+            // Make a sphere (exactly the same as before). 
+            var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
+            var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+            var sphere = new THREE.Mesh(geometry, material)
+      
+            // This time we give the sphere random x and y positions between -500 and 500
+            sphere.position.x = Math.random() * 1000 - 500;
+            sphere.position.y = Math.random() * 1000 - 500;
+            sphere.position.z = Math.random() * 1000 - 500;
+
+      
+            // Then set the z position to where it is in the loop (distance of camera)
+            // sphere.position.z = z;
+      
+            // // scale it up a bit
+            sphere.scale.x = sphere.scale.y = 2;
+      
+            //add the sphere to the scene
+            scene.add( sphere );
+      
+            //finally push it to the stars array 
+            stars.push(sphere); 
+            console.log(stars.length);
+          }
+    }
+  
+    function animateStars() { 
+          
+      // loop through each star
+      for(var i=0; i<stars.length; i++) {
+        
+        var star = stars[i]; 
+          
+        // and move it forward dependent on the mouseY position. 
+        star.position.y +=  i/5;
+          
+        if(star.position.y>1000) star.position.y-=500; 
+        
+      }
+    
+    }
+  
+    function render() {
+      //get the frame
+      requestAnimationFrame( render );
+  
+      //render the scene
+      renderer.render( scene, camera );
+        animateStars();
+  
+    }
+
+
+document.getElementById("but").addEventListener("click", init);
